04ff234b7afdd79c4bbc867a91fb6ec66d1210e6
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-17 19:03:46 +0700

    step 4d sampai step 5

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -6,7 +6,7 @@ int n; //jumlah data yang akan dimasukkan
 
 void input ()
 {
-    
+
 }
 
 void mergeSort(int low, int high){
@@ -25,8 +25,42 @@ void mergeSort(int low, int high){
     int j, k;
     j = mid + 1; // step 4b
     k = low; //step 4c
-
     
+    while (i <= mid && j <= high) //step 4d
+    {
+        if (arr[i] <= arr[j]) //step 4.d.i
+        {
+            B[k] = arr[i];  //step 4.d.i.1
+            i++;            //step 4.d.i.2
+        }
+
+        else
+        {
+            B[k] = arr[j]; //step 4.d.i.3
+            j++;           //step 4.d.i.4
+        }
+        k++; //step 4.d.ii
+    }
+
+    while (j <= high) //step 4e
+    {
+        B[k] = arr[j]; //step 4e.i
+        j++;           //step 4.e.ii
+        k++;           //step 4.e.iii
+    }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i]; //step 4f.i
+        i++;           //step 4f.ii
+        k++;           //step 4f.iii
+    }
+    
+    //step 5
+    for( int x = low; x <= high; x++ )
+    {
+    arr[x] = B[x];
+    }
 }
 
 void output ()
