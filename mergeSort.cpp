7f14c8b43f78e045dbf25d1eeecb7f31b8633121
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-17 19:05:50 +0700

    memasukkan program void output

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -65,7 +65,12 @@ void mergeSort(int low, int high){
 
 void output ()
 {
-
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
